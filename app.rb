44ca9e997c95680c956e294abbbeff7819c5c000
Celina Jackson <celinanjackson@gmail.com>
2020-09-10 20:35:51 +0000

    Automatically backed up by Learn

--- app.rb
+++ app.rb
@@ -2,13 +2,13 @@ require 'sinatra/base'
 
 class App < Sinatra::Base
  get '/newteam' do
-     @name = params[:name]
-    @coach = params[:coach]
-    @pg = params[:pg]
-    @sg = params[:sg]
-    @pf = params[:pf]
-    @sf = params[:sf]
-    @c = params[:c]
+     @name = params["name"]
+    @coach = params["coach"]
+    @pg = params["pg"]
+    @sg = params["sg"]
+    @pf = params["pf"]
+    @sf = params["sf"]
+    @c = params["c"]
    erb :newteam 
  end 
  
